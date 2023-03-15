41bac10ea8a2fec52e89d2fd8e9d5fcb55f84c2c
Adam <Adamkindel1234@gmail.com>
2023-03-15 10:04:57 +0100

    ref(stake): ethers cleanup

--- composables/stake.js
+++ composables/stake.js
@@ -2,8 +2,6 @@ import { ref } from "vue";
 import Web3 from "web3";
 import kryxiviaStakingABI from "@/abi/kryxiviaStakingABI";
 import erc20ABI from "~~/abi/erc20ABI";
-// import { ethers } from "ethers";
-// import { Web3Provider } from "@ethersproject/providers";
 
 export const useStake = () => {
   const { account } = useUser();
@@ -15,23 +13,11 @@ export const useStake = () => {
     }
   });
 
-  const WETHAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
-  const USDCAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
-  const WETHUSDCPoolAddress = "0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640";
-  const WETHKXAPoolAddress = "0x0b1A02b8CaD71BeE94e7fd0845Fd9853Fa1b46ff";
-
   // staking contract
   const stakingContractAddress = "0xbbbe904a9d55c2ecca2abab4111750fc5e59cbf7";
   const contractInstance =
     web3.value &&
     new web3.value.eth.Contract(kryxiviaStakingABI, stakingContractAddress);
-
-  // const providerEthers =
-  //   typeof window !== "undefined" && new Web3Provider(window.ethereum);
-  // const signer = providerEthers && providerEthers.getSigner();
-  // const stakeContractKXAEthers =
-  //   providerEthers &&
-  //   new ethers.Contract(stakingContractAddress, kryxiviaStakingABI, signer);
 
   const kxaContractAddress = "0x2223bF1D7c19EF7C06DAB88938EC7B85952cCd89";
   const kxaContractInstance =
@@ -44,24 +30,6 @@ export const useStake = () => {
   const claimableKXA = computed(() => {
     return stakedKXA.value * waitingPercentAPR.value;
   });
-
-  // const stakeKXA = async (amount, lock) => {
-  //   console.log("amount", amount, lock);
-  //   // allowance for kxa contract
-  //   const allowance = await kxaContractInstance.methods
-  //     .allowance(account.value, stakingContractAddress)
-  //     .call();
-
-  //   if (allowance < amount) {
-  //     const tx = await kxaContractInstance.methods
-  //       .approve(stakingContractAddress, amount)
-  //       .send({ from: account.value });
-  //     console.log("approve tx", tx);
-  //   }
-
-  //   const tx = await stakeContractKXAEthers.stakeKXA(String(amount), false);
-  //   console.log("stake tx", tx);
-  // };
 
   const stakeKXA = async (amount, lock) => {
     console.log("amount", amount, lock);
